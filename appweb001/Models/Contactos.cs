f72ea34dcf18c4f86df4cca0dfb43a7b3400fee1
vrodriguez <vrodriguez@S-VRODRIGUEZ>
2025-05-23 08:17:50 -0600

    Agregar archivos de proyecto.

---
+++ appweb001/Models/Contactos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace appweb001.Models
+{
+    public class Contactos
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoE { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
---
+++ appweb001/Models/ContextoDeDatos.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace appweb001.Models
+{
+    public class ContextoDeDatos: DbContext
+    {
+        public DbSet<Contactos> contactos {  get; set; }
+
+        public ContextoDeDatos(DbContextOptions<ContextoDeDatos> options)
+            : base(options)
+        {
+        }
+
+
+
+
+    }
+}
